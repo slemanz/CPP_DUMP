2a5807f5ca5369eccabc1c82cc902e9447ec7156
slemanz <william_sleman@hotmail.com>
2025-09-05 04:21:06 -0300

    conditionals(feat): add more example

--- COMPREHENSIVE/DEPTH/04_conditional/README.md
+++ COMPREHENSIVE/DEPTH/04_conditional/README.md
@@ -1,1 +1,7 @@
 # Conditional Statements
+
+Conditional statements in C++ allow programs to make decisions and execute
+different blocks of code based on whether specific conditions are true or false.
+These statements are fundamental for controlling the flow of a program. 
+
+- [conditionals.cpp](conditionals.cpp)
--- COMPREHENSIVE/DEPTH/04_conditional/conditionals.cpp
+++ COMPREHENSIVE/DEPTH/04_conditional/conditionals.cpp
@@ -22,5 +22,33 @@ int main()
         cout << "i2 != 5" << endl;
     }
 
+    if(b1)
+    {
+        cout << "b1 is true" << endl;
+    }
+
+    if(!b2)
+    {
+        cout << "b2 is false" << endl;
+    }
+
+    if(b1 || b2)
+    {
+        cout << "  b1 || b2" << endl;
+    }
+
+    if(!(b1 && b2))
+    {
+        cout << "!(b1 && b2)" << endl;
+    }
+
+    if(i1 >= i2)
+    {
+        cout << "i1 greater equal than i2" << endl;
+    }else
+    {
+        cout << "i2 greater than i1" << endl;
+    }
+
     return 0;
 }
