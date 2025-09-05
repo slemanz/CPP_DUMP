1aa9a0e3496a40ecd5b91104e62131fc1740df52
slemanz <william_sleman@hotmail.com>
2025-09-05 04:00:44 -0300

    conditionals(feat): add first examples

--- COMPREHENSIVE/DEPTH/04_conditional/conditionals.cpp
+++ COMPREHENSIVE/DEPTH/04_conditional/conditionals.cpp
@@ -4,6 +4,23 @@ using namespace std;
 
 int main()
 {
+    // make some boolean variables
     bool b1 = true;
     bool b2 = false;
+
+    // make some integer value
+    int i1 = 5;
+    int i2 = 10;
+
+    if(i1 == 5)
+    {
+        cout << "i1 == 5" << endl;
+    }
+
+    if(i2 != 5)
+    {
+        cout << "i2 != 5" << endl;
+    }
+
+    return 0;
 }
