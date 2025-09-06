1ab6d681579ae732c8addec9c520cc75f8882c9c
slemanz <william_sleman@hotmail.com>
2025-09-06 15:51:50 -0300

    memory(feat): finish string

--- COMPREHENSIVE/DEPTH/06_memory_containers/string.cpp
+++ COMPREHENSIVE/DEPTH/06_memory_containers/string.cpp
@@ -11,6 +11,17 @@ int main(void)
     char c = s[0];
     std::cout << c << s[1] << s[2] << s[3] << s[4] << std::endl;
     std::cout << "Length of string 's': " << stringL << std::endl;
+    
+    s[1] = 'a';
+    s[(stringL - 1)] = '.';
+    std::cout << s << std::endl;
+
+    std::string s2 = "Ola";
+    s2 += " Mundo";
+    std::cout << s2 << std::endl;
+
+    s2.append("!!!");
+    std::cout << s2 << std::endl;
 
     return 0;
 }
