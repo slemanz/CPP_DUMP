9cb40e84bfe4e8a81b201ba8332168c7f0c00f94
slemanz <william_sleman@hotmail.com>
2025-09-08 03:43:11 -0300

    loops(feat): add while

--- COMPREHENSIVE/DEPTH/08_loops/README.md
+++ COMPREHENSIVE/DEPTH/08_loops/README.md
@@ -9,3 +9,10 @@ exactly like those we have been using in our conditional statements (if, else
 if, else).
 
 - [while.cpp](while.cpp)
+
+**Keep calm and CTRL-C**
+
+When your programs are careening out of control without any sign of stopping,
+you can always click in your console area and do a CTRL-C key combination (hold
+CTRL and press C). This will kill the current process that is running (which is
+most likely your unstoppable loop program at this point).
--- COMPREHENSIVE/DEPTH/08_loops/while.cpp
+++ COMPREHENSIVE/DEPTH/08_loops/while.cpp
@@ -4,6 +4,29 @@ using namespace std;
 
 int main(void)
 {
+    string password = "password";
+    string input;
+
+    cout << "Enter the password or type 'q' to quit: ";
+    getline(cin, input);
+
+    if(input != password)
+    {
+        while(input != password && input != "q")
+        {
+            cout << "Try again or type 'q' to quit: ";
+            getline(cin, input);
+        }
+    }
+
+    if(input == password)
+    {
+        cout << "YOU GUESSED IT" << endl;
+    }else
+    {
+        cout << "Thank you, come again!" << endl;
+    }
+
 
     return 0;
 }
