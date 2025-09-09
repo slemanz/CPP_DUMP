84c91f216d6060af8f4faa7e43f2fd8563021773
slemanz <william_sleman@hotmail.com>
2025-09-09 03:43:21 -0300

    functions(feat): add first example

--- COMPREHENSIVE/DEPTH/09_functions/function.cpp
+++ COMPREHENSIVE/DEPTH/09_functions/function.cpp
@@ -2,8 +2,29 @@
 
 using namespace std;
 
+int calcPower(int whole, int exp)
+{
+    int result = 1;
+
+    for(int i = 0; i < exp; i++)
+    {
+        result *= whole;
+    }
+    return result;
+}
+
 int main(void)
 {
+    int i1;
+    int i2;
+    cout << "Enter a whole number: ";
+    cin >> i1;
+    cout << "Enter an exponent: ";
+    cin >> i2;
+
+    int answer = calcPower(i1, i2);
+    cout << i1 << "^" << i2 << " = " << answer << endl;
+    
 
     return 0;
 }
