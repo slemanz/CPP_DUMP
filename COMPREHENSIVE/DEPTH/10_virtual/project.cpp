bfe8bf060167f17de7064c0fc0f53226ee6cf002
slemanz <william_sleman@hotmail.com>
2025-09-11 04:59:06 -0300

    virtual(feat): add check board function

--- COMPREHENSIVE/DEPTH/10_virtual/project.cpp
+++ COMPREHENSIVE/DEPTH/10_virtual/project.cpp
@@ -17,6 +17,69 @@
 #include <iomanip>
 
 /*
+    This function checks the entire
+    gameboard to see if there are any
+    winning rows/columns/diagonals.
+    It returns true if there is a winning
+    configuration. Otherwise it returns
+    false.
+*/
+bool checkBoard(char board[][3])
+{
+    bool row_check = true;
+    bool col_check = true;
+    char temp;
+
+    //Check X's on diagonals
+    if((board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') || (board[2][0] == 'X' && board[1][1] == 'X' && board[0][2] == 'X'))
+    {
+        return true;
+    }
+    //Check O's on diagonals
+    else if((board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O') || (board[2][0] == 'O' && board[1][1] == 'O' && board[0][2] == 'O'))
+    {
+        return true;
+    }
+
+    //Check rows
+    for(int r = 0; r < 3; r++)
+    {
+        temp = board[r][0];
+        row_check = true;
+        for(int c = 0; c < 3; c++)
+        {
+            if((board[r][c] != temp) || (board[r][c] == '-'))
+            {
+                row_check = false;
+            }
+        }
+        if(row_check)
+        {
+            return true;
+        }
+    }
+
+    //Check columns
+    for(int c = 0; c < 3; c++)
+    {
+        temp = board[0][c];
+        col_check = true;
+        for(int r = 0; r < 3; r++)
+        {
+            if((board[r][c] != temp) || (board[r][c] == '-'))
+            {
+                col_check = false;
+            }
+        }
+        if(col_check)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+/*
     This function prints the
     gameboard
 */
@@ -61,6 +124,28 @@ int main(void)
         {
             exit(0);
         }
+        std::cout << "Enter Col: ";
+        std::cin >> col;
+
+        // check to see if the player chose a valid position
+        while(board[row-1][col-1] != '-' || (row > 3) || (col > 3))
+        {
+            std::cout << "Invalid choice, please choose again" << std::endl;
+            std::cout << "Enter row: ";
+            std::cin >> row;
+
+            if(row == 0)
+            {
+                exit(0);
+            }
+            std::cout << "Enter Col: ";
+            std::cin >> col;
+        }
+        // asign X or 0 to correct spot on the board
+        board[row-1][col-1] = players[numTurns%2];
+        // Check if game was won and increment the number of turns
+
+        numTurns++;
     }while(1);
 
     return 0;
