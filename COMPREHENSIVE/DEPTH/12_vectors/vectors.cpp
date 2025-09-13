5e941112b18d31019764b5a07995b8b67c4d90f9
slemanz <william_sleman@hotmail.com>
2025-09-13 08:37:24 -0300

    vectors(feat): add content to file example

--- COMPREHENSIVE/DEPTH/12_vectors/vectors.cpp
+++ COMPREHENSIVE/DEPTH/12_vectors/vectors.cpp
@@ -7,6 +7,24 @@ int main(void)
 {
     vector<int> myVect;
 
+    cout << "myVect size: " << myVect.size() << endl;
+
+    myVect.push_back(3);
+
+    cout << myVect[0] << endl;
+    cout << "myVect size: " << myVect.size() << endl;
+    myVect.push_back(6);
+    myVect.push_back(7);
+    myVect.push_back(3);
+
+    cout << "myVect size: " << myVect.size() << endl;
+    myVect.pop_back();
+    cout << "myVect size after pop: " << myVect.size() << endl;
+    cout << myVect.back() << endl;
+
+    myVect.clear();
+    cout << "myVect size after clear: " << myVect.size() << endl;
+
     return 0;
 }
 
