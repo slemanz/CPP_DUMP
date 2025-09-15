6e609d659af6f26880c2104dcd9515ba579509f2
slemanz <william_sleman@hotmail.com>
2025-09-15 04:28:32 -0300

    adt(feat): add quick challenge

--- COMPREHENSIVE/DEPTH/16_adt/quick_challenge.cpp
+++ COMPREHENSIVE/DEPTH/16_adt/quick_challenge.cpp
@@ -11,8 +11,39 @@
 
 using namespace std;
 
-int main(void)
+struct Person{
+    string name;
+    int age;
+    float weight;
+};
+
+int main(int argc, char *argv[])
 {
+    if(argc != 2)
+    {
+        cout << "Usage ./filename <inputfile>" << endl;
+        exit(-1);
+    }
+
+    ifstream myStream;
+    myStream.open(argv[1]);
+
+    vector<Person> personVect;
+
+    Person p;
+
+    while(myStream >> p.name >> p.age >> p.weight)
+    {
+        personVect.push_back(p);
+    }
+
+    for(int i = 0; i < personVect.size(); i++)
+    {
+        cout << "name: " << personVect[i].name << endl;
+        cout << "age: " << personVect[i].age << endl;
+        cout << "weight: " << personVect[i].weight << endl << endl;
+
+    }
 
     return 0;
 }
