2ca4c9f12de932bd0d7aa9dd1d01a25842b4bf60
slemanz <william_sleman@hotmail.com>
2025-09-16 04:31:12 -0300

    oop(feat): add car struct

--- COMPREHENSIVE/DEPTH/17_oop/car.cpp
+++ COMPREHENSIVE/DEPTH/17_oop/car.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 
+class Car{
+    public:
+        // member function
+        void driveCar(int miles);
+        void setPaint(std::string color);
+        int getOdometer();
+        float getFuel();
+        void addFuel(int gallons);
+    
+    private:
+        // member variables
+        float fuel;
+        std::string paintColor;
+        std::string model;
+        int odometer;
+};
 
 int main(void)
 {
