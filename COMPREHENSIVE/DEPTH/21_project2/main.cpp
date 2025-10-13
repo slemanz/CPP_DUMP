7f87ca77c7ff7346c7773a0052fdcf7b989f9201
slemanz <william_sleman@hotmail.com>
2025-10-13 04:03:48 -0300

    project(feat): add shape in main

--- COMPREHENSIVE/DEPTH/21_project2/main.cpp
+++ COMPREHENSIVE/DEPTH/21_project2/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include "Shape.hpp"
 
 int main(void)
 {
     std::cout << "hello world!" << std::endl;
+
+    Shape shape1(5, 10 , "blue");
+    std::cout << "SHAPE1 COORDS: " << shape1.getX() << ", ";
+     std::cout << shape1.getY() << "\n" << std::endl;
+
 
     return 0;
 }
